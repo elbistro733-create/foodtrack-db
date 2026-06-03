5f26612b9f864796812da287bcf370a8fba49744
Florencia <elbistro733@gmail.com>
2026-06-03 16:40:22 -0300

    Agrego claves foraneas y restricciones

--- scripts/1er TP SQL.sql
+++ scripts/1er TP SQL.sql
@@ -35,3 +35,41 @@ CREATE TABLE products (
 SELECT table_name 
 FROM information_schema.tables 
 WHERE table_schema = 'public';
+
+
+ALTER TABLE orders
+ADD COLUMN delivery_status VARCHAR(50);
+
+ALTER TABLE orders
+ADD CONSTRAINT check_delivery  -- es la restriccion para que no acepta nada
+CHECK (delivery_status IN ('En tiempo', 'Demorado'));  --check: solo acepte estos 2 valores
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_foodtruck
+FOREIGN KEY (foodtruck_id) 
+REFERENCES foodtrucks(foodtruck_id);
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_foodtruck
+FOREIGN KEY (foodtruck_id) 
+REFERENCES foodtrucks(foodtruck_id);
+
+ALTER TABLE products
+ADD CONSTRAINT fk_products_foodtruck
+FOREIGN KEY (foodtruck_id) 
+REFERENCES foodtrucks(foodtruck_id);
+
+ALTER TABLE order_items
+ADD CONSTRAINT fk_orderitems_order
+FOREIGN KEY (order_id) 
+REFERENCES orders(order_id);
+
+ALTER TABLE order_items
+ADD CONSTRAINT fk_orderitems_product
+FOREIGN KEY (product_id) 
+REFERENCES products(product_id);
+
+ALTER TABLE locations
+ADD CONSTRAINT fk_locations_foodtruck
+FOREIGN KEY (foodtruck_id) 
+REFERENCES foodtrucks(foodtruck_id);
